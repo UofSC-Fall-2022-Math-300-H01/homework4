18ffdd67e01ee795380e680d33115bc414d9f420
Matthew Ballard <matt@mrb.email>
2022-09-14 16:44:08 -0400

    final setup

--- Hw4.lean
+++ Hw4.lean
@@ -1,17 +1,16 @@
-
 variable (P Q: Prop)
 
 -- Problem 1 
-theorem problem1 : True := trivial 
+theorem problem1 : True := sorry 
 
 -- Problem 2 
-theorem problem2 (h : P) : Q → P := fun (_:Q) => h 
+theorem problem2 (h : P) : Q → P := sorry
 
 -- Problem 3 
-theorem problem3 : P → Q → P := fun (p:P) => fun (_:Q) => p 
+theorem problem3 : P → Q → P := sorry
 
 -- Problem 4 
-theorem problem4 (h : P ∧ Q) : P ∨ Q := Or.inl (And.left h) 
+theorem problem4 (h : P ∧ Q) : P ∨ Q := sorry
 
 -- Problem 5 
-theorem problem5 : (P ↔ Q) → (P → Q) := fun (h:P ↔ Q) => fun (p:P) => Iff.mp h p 
+theorem problem5 : (P ↔ Q) → (P → Q) := sorry
